fbd0f17f663c7e04f32350958292ccc330d6815c
ElysiuM1617 <benitesjared12@gmail.com>
2025-03-30 10:24:33 -0500

    feat(js): se implementa funcion para extraer el Youtube ID

--- script.js
+++ script.js
@@ -6,3 +6,14 @@ const widgetContainer = document.getElementById("widgetContainer");
 generateBtn.addEventListener("click", () => {
     console.log("Generar widget presionado");
 });
+
+async function extractId(url) {
+    let match = url.match(/youtube\.com\/channel\/([\w-]+)/);
+    if (match) return { type: "channel", id: match[1] };
+
+    match = url.match(/(?:youtube\.com\/watch\?v=|youtu\.be\/)([\w-]+)/);
+    if (match) return { type: "video", id: match[1] };
+
+    return null;
+}
+
