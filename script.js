73f9c584b6aff85a5a571a11f9a8fc50d4b7fd9c
ElysiuM1617 <benitesjared12@gmail.com>
2025-03-30 10:22:50 -0500

    feat(js): agrega la estructura basica js

---
+++ script.js
@@ -0,0 +1,8 @@
+const widgetType = document.getElementById("widgetType");
+const youtubeUrl = document.getElementById("youtubeUrl");
+const generateBtn = document.getElementById("generateWidget");
+const widgetContainer = document.getElementById("widgetContainer");
+
+generateBtn.addEventListener("click", () => {
+    console.log("Generar widget presionado");
+});
